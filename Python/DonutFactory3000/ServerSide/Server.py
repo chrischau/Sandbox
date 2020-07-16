73ae27e40198f0abd8fc43a4d23b3361bbfc9f5e
Chris Chau <christopher.chau@gmail.com>
2020-07-16 14:09:47 +0900

    connect mssql db to retrieve data, return json

--- Python/DonutFactory3000/ServerSide/Server.py
+++ Python/DonutFactory3000/ServerSide/Server.py
@@ -1,41 +1,89 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-#from sqlalchemy import create_engine
-from json import dumps
+from sqlalchemy import create_engine
 from flask_jsonpify import jsonify
+import json
+import sys
+import traceback
+import logging
+import pyodbc
 
-#db_connect = create_engine('sqlite:///chinook.db')
+
 app = Flask(__name__)
 api = Api(app)
+connectionString = "Driver={SQL Server};Server=SINWS0127;Database=Company;UID=sa;PWD=changeme;"
 
 class Employees(Resource):
-    def get(self):
-        #conn = db_connect.connect() # connect to database
-        #query = conn.execute("select * from employees") # This line performs query and returns json result
-        #return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    def __init__(self):
+        logging.basicConfig(filename="C:\Development\GitHub\Sandbox\Python\DonutFactory3000\ServerSide\server_trace.log",
+                            filemode='a',
+                            format='%(asctime)s.%(msecs)d-%(name)s-%(levelname)s-%(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
+
+        logging.info("Running logger in Employees constructor") #seems like it has to be in this order, before assigning to self.logger, weird and not sure why
+        self.logger = logging.getLogger(__name__)
         
-        return {'You have successfully called GET'}        
+        self.dbConnection = pyodbc.connect(connectionString)
 
-class Tracks(Resource):
     def get(self):
-        #conn = db_connect.connect()
-        #query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        #result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        result = {'data' : 'a, b, c, d', 'names' : 'john, mary, simon'}
-        return jsonify(result)
+        try: 
+            self.logger.info("Inside Employees get function")
+            
+            cursor = self.dbConnection.cursor()
+            cursor.execute("SELECT * FROM Employees")
+      
+            columns = [column[0] for column in cursor.description]
+            results = []
+            for row in cursor.fetchall():
+                results.append(dict(zip(columns, row)))
+            
+            #print(results)
+            self.logger.info(results)
+            
+            cursor.close()
+            return jsonify(results)
+            
 
+        except Exception as ex:
+            self.logger.error("Operation Failed:\n" + str(ex))
+            
 class Employees_Name(Resource):
-    def get(self, employee_id):
-        #conn = db_connect.connect()
-        #query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        #result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        result = {'data' : [ {'address' : 'abc', 'name' : 'john', 'city' : 'junkjunk'}]}
-        return jsonify(result)
-        
+    def __init__(self):
+        logging.basicConfig(filename="C:\Development\GitHub\Sandbox\Python\DonutFactory3000\ServerSide\server_trace.log",
+                            filemode='a',
+                            format='%(asctime)s.%(msecs)d-%(name)s-%(levelname)s-%(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+        logging.info("Running logger in Employees Name constructor")
+        self.logger = logging.getLogger(__name__)
+        
+        self.dbConnection = pyodbc.connect(connectionString)
+
+    def get(self, employee_id):
+        try:
+            self.logger.info("Inside Employees Name get function")
+                
+            cursor = self.dbConnection.cursor()
+            cursor.execute("SELECT * FROM Employees WHERE EmployeeId = %d" %int(employee_id))
+        
+            columns = [column[0] for column in cursor.description]
+            results = []
+            for row in cursor.fetchall():
+                results.append(dict(zip(columns, row)))
+            
+            #print(results)
+            self.logger.info(results)
+            
+            cursor.close()
+            return jsonify(results)
+        
+        except Exception as ex:
+            self.logger.error("Operation Failed:\n" + str(ex))
+
+api.add_resource(Employees, '/employees')
+api.add_resource(Employees_Name, '/employees/<employee_id>')
 
 
 if __name__ == '__main__':
