1a603ccbfd25038f9bd65fcf6c358e68f026af49
Chris Chau <christopher.chau@gmail.com>
2020-07-21 22:58:10 +0900

    updated the data retrieval to DataLayer class

--- Python/DonutFactory3000/ServerSide/Server.py
+++ Python/DonutFactory3000/ServerSide/Server.py
@@ -7,15 +7,12 @@ import sys
 import traceback
 import logging
 import pyodbc
-
+from DataLayer import UserData
 
 app = Flask(__name__)
 api = Api(app)
 connectionString = "Driver={SQL Server};Server=SINWS0127;Database=EscrowService;UID=sa;PWD=changeme;"
 
-users = [{"name" : "Nicholas", "age" : 42, "occupation" : "Network Engineer"},
-         {"name" : "Elvin", "age" : 32, "occupation" : "Doctor"},
-         {"name" : "Jass", "age" : 22, "occupation" : "Web Developer"}]
          
 class Users(Resource):
     def __init__(self):
@@ -27,111 +24,56 @@ class Users(Resource):
 
         logging.info("Running logger in Users constructor") #seems like it has to be in this order, before assigning to self.logger, weird and not sure why
         self.logger = logging.getLogger(__name__)
-        
         #self.dbConnection = pyodbc.connect(connectionString)
+        self.userData = UserData()
 
     def get(self, name):
         try: 
-            #self.logger.info("Inside Users get function")
-            
-            # cursor = self.dbConnection.cursor()
-            # cursor.execute("SELECT * FROM Users")
-      
-            # columns = [column[0] for column in cursor.description]
-            # results = []
-            # for row in cursor.fetchall():
-            #     results.append(dict(zip(columns, row)))
-            
-            # #print(results)
-            # self.logger.info(results)
-            
-            # cursor.close()
-            # return jsonify(results)
-            
-            if not name.strip():
-                return users, 200
-
-            for user in users:
-                if (user["name"] == name):
-                    return user, 200
-            
-            return "User not found", 404
+            users = self.userData.FindUser(name)
+            return users, 200
 
         except Exception as ex:
             self.logger.error("Operation Failed:\n" + str(ex))
+            return str(ex), 404
 
-    
-    # def get(self, user_id):
-    #     try:
-    #         self.logger.info("Inside Find User By Id get function")
-                
-    #         cursor = self.dbConnection.cursor()
-    #         cursor.execute("SELECT * FROM Users WHERE UserId = %d" %int(user_id))
-        
-    #         columns = [column[0] for column in cursor.description]
-    #         results = []
-    #         for row in cursor.fetchall():
-    #             results.append(dict(zip(columns, row)))
-            
-    #         #print(results)
-    #         self.logger.info(results)
-            
-    #         cursor.close()
-    #         return jsonify(results)
-        
-    #     except Exception as ex:
-    #         self.logger.error("Operation Failed:\n" + str(ex))
 
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
+        try:
+            self.userData.AddUser(name)
+            return "User {} has been added successfully".format(name), 201
 
-        for user in users:
-            if (name == user["name"]):
-                return "User with name {} already exists.".format(name), 400
+        except Exception as ex:
+            self.logger.error("Operation Failed:\n" + str(ex))
+            return str(ex), 400
 
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]            
-        }
-
-        users.append(user)
-        return user, 201
-    
 
     def delete(self, name):
-        for user in users:
-            if (user["name"] == name):
-                users.remove(user)
-                return "{} is deleted.".format(name), 200
-
-        return "User not found", 404
+        try:
+            result = self.userData.RemoveUser(name)
+            return result, 200
+        
+        except Exception as ex:
+            self.logger.error("Operation Failed:\n" + str(ex))
+            return str(ex), 404
+        
 
     
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if (name == user["name"]):
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]            
-        }
-
-        users.append(user)
-        return user, 201
-
+        try:
+            result = self.userData.UpdateUser(name)
+            
+            if (result == 1):
+                return "User {} is successfully updated.".format(name), 200
+            elif (result == 0):
+                return "User {} is successfully added.".format(name), 201
+            else:
+                raise NameError("Unknown error occurred")
+    
+        except Exception as ex:
+            self.logger.error("Operation Failed:\n" + str(ex))
+            return str(ex), 404
+        
+        
             
 
 
