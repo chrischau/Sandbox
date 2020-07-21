a28bcf9157d0b44675c89c30dc60ec8de8b31abb
Chris Chau <christopher.chau@gmail.com>
2020-07-21 21:59:38 +0900

    return data from memory, trying out POST, PUT, and DELETE operations

--- Python/DonutFactory3000/ServerSide/Server.py
+++ Python/DonutFactory3000/ServerSide/Server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 from flask_jsonpify import jsonify
 import json
@@ -11,9 +11,13 @@ import pyodbc
 
 app = Flask(__name__)
 api = Api(app)
-connectionString = "Driver={SQL Server};Server=SINWS0127;Database=Company;UID=sa;PWD=changeme;"
+connectionString = "Driver={SQL Server};Server=SINWS0127;Database=EscrowService;UID=sa;PWD=changeme;"
 
-class Employees(Resource):
+users = [{"name" : "Nicholas", "age" : 42, "occupation" : "Network Engineer"},
+         {"name" : "Elvin", "age" : 32, "occupation" : "Doctor"},
+         {"name" : "Jass", "age" : 22, "occupation" : "Web Developer"}]
+         
+class Users(Resource):
     def __init__(self):
         logging.basicConfig(filename="C:\Development\GitHub\Sandbox\Python\DonutFactory3000\ServerSide\server_trace.log",
                             filemode='a',
@@ -21,69 +25,118 @@ class Employees(Resource):
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
 
-        logging.info("Running logger in Employees constructor") #seems like it has to be in this order, before assigning to self.logger, weird and not sure why
+        logging.info("Running logger in Users constructor") #seems like it has to be in this order, before assigning to self.logger, weird and not sure why
         self.logger = logging.getLogger(__name__)
         
-        self.dbConnection = pyodbc.connect(connectionString)
+        #self.dbConnection = pyodbc.connect(connectionString)
 
-    def get(self):
+    def get(self, name):
         try: 
-            self.logger.info("Inside Employees get function")
+            #self.logger.info("Inside Users get function")
             
-            cursor = self.dbConnection.cursor()
-            cursor.execute("SELECT * FROM Employees")
+            # cursor = self.dbConnection.cursor()
+            # cursor.execute("SELECT * FROM Users")
       
-            columns = [column[0] for column in cursor.description]
-            results = []
-            for row in cursor.fetchall():
-                results.append(dict(zip(columns, row)))
+            # columns = [column[0] for column in cursor.description]
+            # results = []
+            # for row in cursor.fetchall():
+            #     results.append(dict(zip(columns, row)))
             
-            #print(results)
-            self.logger.info(results)
+            # #print(results)
+            # self.logger.info(results)
             
-            cursor.close()
-            return jsonify(results)
+            # cursor.close()
+            # return jsonify(results)
             
+            if not name.strip():
+                return users, 200
+
+            for user in users:
+                if (user["name"] == name):
+                    return user, 200
+            
+            return "User not found", 404
 
         except Exception as ex:
             self.logger.error("Operation Failed:\n" + str(ex))
-            
-class Employees_Name(Resource):
-    def __init__(self):
-        logging.basicConfig(filename="C:\Development\GitHub\Sandbox\Python\DonutFactory3000\ServerSide\server_trace.log",
-                            filemode='a',
-                            format='%(asctime)s.%(msecs)d-%(name)s-%(levelname)s-%(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.INFO)
 
-        logging.info("Running logger in Employees Name constructor")
-        self.logger = logging.getLogger(__name__)
-        
-        self.dbConnection = pyodbc.connect(connectionString)
-
-    def get(self, employee_id):
-        try:
-            self.logger.info("Inside Employees Name get function")
+    
+    # def get(self, user_id):
+    #     try:
+    #         self.logger.info("Inside Find User By Id get function")
                 
-            cursor = self.dbConnection.cursor()
-            cursor.execute("SELECT * FROM Employees WHERE EmployeeId = %d" %int(employee_id))
+    #         cursor = self.dbConnection.cursor()
+    #         cursor.execute("SELECT * FROM Users WHERE UserId = %d" %int(user_id))
         
-            columns = [column[0] for column in cursor.description]
-            results = []
-            for row in cursor.fetchall():
-                results.append(dict(zip(columns, row)))
+    #         columns = [column[0] for column in cursor.description]
+    #         results = []
+    #         for row in cursor.fetchall():
+    #             results.append(dict(zip(columns, row)))
             
-            #print(results)
-            self.logger.info(results)
+    #         #print(results)
+    #         self.logger.info(results)
             
-            cursor.close()
-            return jsonify(results)
+    #         cursor.close()
+    #         return jsonify(results)
         
-        except Exception as ex:
-            self.logger.error("Operation Failed:\n" + str(ex))
+    #     except Exception as ex:
+    #         self.logger.error("Operation Failed:\n" + str(ex))
 
-api.add_resource(Employees, '/employees')
-api.add_resource(Employees_Name, '/employees/<employee_id>')
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if (name == user["name"]):
+                return "User with name {} already exists.".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]            
+        }
+
+        users.append(user)
+        return user, 201
+    
+
+    def delete(self, name):
+        for user in users:
+            if (user["name"] == name):
+                users.remove(user)
+                return "{} is deleted.".format(name), 200
+
+        return "User not found", 404
+
+    
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if (name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]            
+        }
+
+        users.append(user)
+        return user, 201
+
+            
+
+
+api.add_resource(Users, '/users/<string:name>')
+#api.add_resource(Users, '/users/<user_id>')
 
 
 if __name__ == '__main__':
