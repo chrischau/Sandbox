6261c82842b717116c5229032c32052a80ce0298
Chris Chau <christopher.chau@gmail.com>
2020-08-26 19:18:05 +0900

    playing with Closure in GO with Fibonacci

--- Go/main.go
+++ Go/main.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Bootajay")
+	// fmt.Println("Bootajay")
 
-	var abc, def = false, 123
+	// var abc, def = false, 123
 
-	fmt.Println(abc)
-	fmt.Println(def)
+	// fmt.Println(abc)
+	// fmt.Println(def)
+
+	f := fibonacci()
+	for i := 0; i < 15; i++ {
+		fmt.Println(f())
+	}
+}
+
+func fibonacci() func() int {
+	first, second := 0, 1
+
+	return func() int {
+		ret := first
+		first, second = second, first+second
+		return ret
+	}
 }
