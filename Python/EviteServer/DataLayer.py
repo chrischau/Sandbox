d794800b9f85f1758726d73f8efcccf53b51addc
Chris Chau <christopher.chau@gmail.com>
2020-10-02 14:56:48 +0900

    added CreateAttendee, more validation before saving, try catch block to rollback failed DB transaction

--- Python/EviteServer/DataLayer.py
+++ Python/EviteServer/DataLayer.py
@@ -3,21 +3,33 @@
 from flask import Flask, json
 import datetime
 import sqlite3
+import re
 
 
 class DataLayer:  
   databaseName = "Database\EventServer.db"
+
+  # Event related SQLs
   findAllEventsSQL = "SELECT EventId, EventName, Location, StartTime, EndTime FROM Events"
   createEventSQL = "INSERT INTO Events(EventName, Location, StartTime, EndTime) VALUES ('{}', '{}', '{}', '{}')"
+    
+  # Attendee related SQLs
+  findAllAttendeeSQL = "SELECT AttendeeEmail FROM Attendees"
+  createAttendeeSQL = "INSERT INTO Attendees(AttendeeEmail) VALUES ('{}')"
 
+  # EventsXAttendees related SQLs
+  findAllEXASQL = "SELECT EventId, AttendeeId FROM EventsXAttendees"
+  createEXASQL = "INSERT INTO EventsXAttendee(EventId, AttendeeId) VALUES ('{}', '{}')"
+
+  
   def __init__(self):
     ## TODO add logging
     self.sqliteConnection = sqlite3.connect(self.databaseName)
     self.cursor = self.sqliteConnection.cursor()
     print("Database created and Successfully Connected to SQLite")
 
-    self.cursor.execute("select sqlite_version();")
-    print("SQLite Database Version is: ", self.cursor.fetchall())
+    # self.cursor.execute("select sqlite_version();")
+    # print("SQLite Database Version is: ", self.cursor.fetchall())
 
 
   def FindAllEvents(self):
@@ -32,6 +44,7 @@ class DataLayer:
   def FindEvent(self, eventId, eventName):
     #self.logger.info("Inside Users get function")
 
+    #TODO paging data if too many data at once
     if (eventId != None):
       whereClause = " WHERE EventId = {}".format(eventId)
       self.cursor.execute(self.findAllEventsSQL + whereClause)
@@ -49,33 +62,71 @@ class DataLayer:
     else:
       return None
 
-  def ValidateTimeFormat(self, timeStamp):
+
+  def __ValidateTimeFormat(self, timeStamp, errorMessage):
     try:
       datetime.datetime.strptime(timeStamp, '%Y-%m-%d %H:%M:%S')
     except ValueError:
-      return False
-    
-    return True
+      raise ValueError("Incorrect datetime format.  It should be YYYY-MM-DD HH:MM:SS")
     
 
   def CreateEvent(self, eventName, location, startTime, endTime):
-    #TODO test, assume time format are set correct when passed in
-    if self.ValidateTimeFormat(startTime) and self.ValidateTimeFormat(endTime):
-      insertStatement = self.createEventSQL.format(eventName, location, startTime, endTime)
-    else:
-      raise ValueError("Incorrect datetime format.  It should be YYYY-MM-DD HH:MM:SS")
+    sqlStatement = self.findAllEventsSQL + " WHERE EventName = '{}'".format(eventName)
+    
+    self.__ValidateRecordDoesNotExist(sqlStatement, "Event with the same name '{}' already exist.  Please choose another name.".format(eventName))
+    self.__ValidateTimeFormat(startTime, "Incorrect Start Time datetime format.  It should be YYYY-MM-DD HH:MM:SS")
+    self.__ValidateTimeFormat(endTime, "Incorrect End Time datetime format.  It should be YYYY-MM-DD HH:MM:SS")
 
-    count = self.cursor.execute(insertStatement)
-    self.sqliteConnection.commit()
+    insertStatement = self.createEventSQL.format(eventName, location, startTime, endTime)
 
-    return count
+    try:
+      self.cursor.execute(insertStatement)
+      self.sqliteConnection.commit()
+
+    except Exception as ex:
+      self.sqliteConnection.rollback()
+      raise ValueError("An error has occurred on the database interaction.  Changes have been rolled back.  \nError Message:" + str(ex))
+
+
+  def CreateAttendee(self, email):
+    sqlStatement = self.findAllAttendeeSQL + " WHERE AttendeeEmail = '{}'".format(email)
+    
+    self.__ValidateRecordDoesNotExist(sqlStatement, "Attendee with the same email '{}' already exist.  Please verify and adjust accordingly.".format(email))
+    self.__ValidateEmailStructure(email)    
+
+    insertStatement = self.createAttendeeSQL.format(email)
+
+    try:
+      self.cursor.execute(insertStatement)
+      self.sqliteConnection.commit()
+
+    except Exception as ex:
+      self.sqliteConnection.rollback()
+      raise ValueError("An error has occurred on the database interaction.  Changes have been rolled back.  \nError Message:" + str(ex))
 
 
 
-# data = DataLayer()
+  def __ValidateEmailStructure(self, email):
+    emailRegex = "[^@]+@[^@]+\.[^@]+"
+
+    if (not re.match(emailRegex, email)):  
+      raise ValueError("Email provided '{}' is not a valid structured email.", email)     
+        
+
+  def __ValidateRecordDoesNotExist(self, sqlStatement, errorMessage):
+    self.cursor.execute(sqlStatement)
+    row = self.cursor.fetchone()
+    if (row is not None):
+      raise ValueError(errorMessage)
+    
+
+
+
+
 # print(data.FindEvent(1, None))
 
-# data = EventsData()
 # print(data.FindAllEvents())
+data = DataLayer()
+#print(data.CreateEvent("American Social Meeting 2", "Tokyo", "2020-10-02 20:00:00", "2020-10-03 02:00:00"))
 
-# print(data.CreateEvent("Tokyo Midnight Outing", "Tokyo", "2020-10-02 20:00:00", "2020-10-03 02:00:00"))
+data.CreateAttendee("joseph@abc.com")
